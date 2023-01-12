7d43aaf84172f4fc45ab1940cc9344d0cf2a8c3e
Josué Silva <josueluzsilva.01@gmail.com>
2023-01-12 00:47:35 -0300

    comentario

--- códigos_individuais_cards/limpar-input.js
+++ códigos_individuais_cards/limpar-input.js
@@ -1,5 +1,5 @@
 // Alterar o comportamento do formulário para que os campos sejam limpos após o usuário clicar no botão calcular.
 
 function limparInput() { // add função ao "button", com a propriedade "onclick"
-    document.getElementById("myForm").reset(); // add "id" como propriedade ao formulário (<form>).
+    document.getElementById("myForm").reset(); // add "id='myForm'" como propriedade ao formulário (<form>).
 }
